2feac95cfdc5e82b84654d8c8c45cbd576326427
nwarlen <warlen15@up.edu>
2014-11-03 10:21:51 -0800

    added mockups for design document

--- server/www/controllers.js
+++ server/www/controllers.js
@@ -35,3 +35,57 @@ scioServices.factory("ExperimentNames", function ($resource) {
         {get : {method : 'GET', params : {}, isArray:true}}
     )
 });
+
+$(function() {
+    $('#highcharts').highcharts({
+        chart: {
+            zoomType: 'x'
+        },
+        title: {
+            text: 'Visual Data'
+        },
+        subtitle: {
+            text: 'Zak:Pearson'
+        },
+        xAxis: {
+            type: 'datetime',
+            minRange: 3600000 // fourteen days
+        },
+        yAxis: {
+            title: {
+                text: 'value'
+            }
+        },
+        legend: {
+            enabled: false
+        },
+        plotOptions: {
+            area: {
+                fillColor: {
+                    linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1},
+                    stops: [
+                        [0, Highcharts.getOptions().colors[0]],
+                        [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                    ]
+                },
+                marker: {
+                    radius: 2
+                },
+                lineWidth: 1,
+                states: {
+                    hover: {
+                        lineWidth: 1
+                    }
+                },
+                threshold: null
+            }
+        },
+        series: [{
+            type: 'area',
+            name: 'Temp Data',
+            pointInterval: 60000,
+            pointStart:Date.UTC(2014,9,28,13,57),
+            data: [20.0,20.0,20.0,20.0,20.25]
+        }]
+    });
+});
