f98ee336452ad00c1ac9ef0ab620f3359f51b046
nwarlen <warlen15@up.edu>
2015-03-04 14:16:24 -0800

    Removed preg-match and plain text POST

--- box/public/js/cloud.js
+++ box/public/js/cloud.js
@@ -1,8 +1,5 @@
-
-
-var bt = bt || {};
-
-bt.cloudLogin = function() {
+var cloudForm = document.getElementById('cloud_submit');
+cloudForm.addEventListener('click', function() {
     //Get login info
     var user = document.getElementById('cloud_user').value;
     var pass = document.getElementById('cloud_pass').value;
@@ -18,14 +15,14 @@ bt.cloudLogin = function() {
     console.log(JSON.stringify(data));
 
     var xhr = new XMLHttpRequest();
-    xhr.open('POST','http://54.69.58.101/api/authUser', true);
-    xhr.setRequestHeader('Content-Type', 'text/plain; charset=UTF-8');
+    xhr.open('POST','http://ec2-54-69-58-101.us-west-2.compute.amazonaws.com/api/authUser', true);
+    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
     xhr.send(JSON.stringify(data));
     xhr.onloadend = function () {
         console.log(xhr.responseText);
     };
 
     return false;
-};
+});
 
 
